7da98d39ee01bb00d7747a6cdebc742f4823fbce
KevinsVision <US.Kevin@iCloud.com>
2021-06-05 01:49:43 -0700

    created database

--- db/migrate/20210605083600_create_users.rb
+++ db/migrate/20210605083600_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer :id
+      t.integer :msg_id
       t.text :message
       t.integer :count
 
-      t.timestamps
+      # t.timestamps
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+two = User.create(id: '001', message: 'One Two')
+three = User.create(id: '002', message: 'One Two Three')
+four = User.create(id: '003', message: 'One Two Three Four')
+five = User.create(id: '004', message: 'One Two Three Four Five')
+six = User.create(id: '005', message: 'One Two Three Four Five Six')
+seven = User.create(id: '006', message: 'One Two Three Four Five Six Seven')
