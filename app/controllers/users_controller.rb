6379be166575c9435486c533114cddb8cefac26f
KevinsVision <US.Kevin@iCloud.com>
2021-06-07 03:06:23 -0700

    added CREATE method with params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(allowed_params)
+  
+    if @user.save
+      redirect_to users_path
+    else
+      flash[:alert] = "ID already exists"
+      render 'new'
+    end
+  end
+  
+  private
+    def allowed_params
+      params.require(:user).permit(:msg_id, :message)
+    end
+
 end
