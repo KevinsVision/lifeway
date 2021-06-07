65a2509d59784ecb12d23879b51dbfa50a8ef48d
KevinsVision <US.Kevin@iCloud.com>
2021-06-07 03:04:02 -0700

    excluded extra attributes from JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     users = User.all
     # call in the new method (word_count) created in the user model
-    render json: users.as_json(methods: :word_count)
+    render json: @users.as_json(methods: [:count], :except => [:message, :msg_id, :user_id, :word_count, :id])
   end
 
 end
